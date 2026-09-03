f5409904c1aed02bd4a309d08ba6fa00b6d4af0a
Rohineesh Bandari <22211a6609@gmail.com>
2026-09-03 14:55:16 +0530

    Secure database credentials

--- FoodApp/src/main/java/com/foodapp/util/DBConnection.java
+++ FoodApp/src/main/java/com/foodapp/util/DBConnection.java
@@ -5,24 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	
-	private static final String URL = "jdbc:mysql://localhost:3306/food";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "123$5678";
-	private static Connection connection = null;
-	
-	public static final Connection getConnection(){
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
 
-			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return connection;
-	}
+    private static final String URL = "jdbc:mysql://localhost:3306/food";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static Connection connection = null;
+
+    public static final Connection getConnection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
